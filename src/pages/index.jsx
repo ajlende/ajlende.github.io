57b7980be2359e63fdf0b4e438365b8c78bf2a85
Alex Lende <ajlende@gmail.com>
2020-01-11 15:49:15 -0600

    Update bio on homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -26,16 +26,17 @@ const Prose = () => (
     <Title hasTextAlign="centered">Welcome!</Title>
     <Content>
       <p>
-        I'm a Senior Software Engineer, creating 3D data visualizations with WebGL, composing
-        data-driven soundtracks with the Web Audio API, and exploring the future of health and care
-        by prototyping out smaller projects for <a href="https://www.cerner.com">Cerner</a> in
-        Kansas City. You can find me most places on the internet as @ajlende.
+        I'm a Software Engineer for <a href="https://automattic.com">Automattic</a> where I tinker
+        with various projects to explore and expand what can be done on the web on platforms like
+        WordPress. You can find me most places on the internet as @ajlende.
       </p>
       <p>
         I love learning, and I think that's why the ever-changing landscape of frontend web
-        development has been so appealing to me. But learning isn't any good unless you share your
-        knowledge with others—I've given few talks at local meetups and a local conference to share
-        some of my knowledge.{" "}
+        development has been so appealing to me. I like to push the limits of what can be done in a
+        web browser, from building virtual reality experiences with the WebXR API to generating
+        music with the Web Audio API to exploring other programming languages that can compile and
+        run on the web like Elm and Rust. I've given few talks at local meetups and a local
+        conference to talk about some of the stuff that I like to do.{" "}
         <a href="https://github.com/ajlende/elm-webgl-presentation">WebGL in Elm</a> and{" "}
         <a href="https://github.com/ajlende/geospacial-data-talk">Geospacial Data Visualizations</a>{" "}
         both have the slides and speaker notes available on my{" "}
@@ -44,16 +45,7 @@ const Prose = () => (
       <p>
         Outside of tech, I enjoy The Great Outdoors™, playing ukulele, and exploring the world. I
         achieved the rank of Eagle Scout while in the Boy Scouts of America, and still enjoy camping
-        as often as I can. My more memorable adventures have been{" "}
-        <a href="http://www.iowastatedaily.com/ames247/article_c384cfa8-2be5-11e6-bb4e-87b03dd281f6.html">
-          performing in France
-        </a>{" "}
-        for D-Day memorial ceremonies, the{" "}
-        <a href="https://ragbrai.com/">Des Moines Register's Annual Great Bike Ride Across Iowa</a>{" "}
-        (RAGBRAI), a 10-day Vipassana meditation course in{" "}
-        <a href="http://www.giri.dhamma.org/">Dhamma Giri, India</a>, and a 10-day canoeing trip in
-        Ontario's beautiful{" "}
-        <a href="https://en.wikipedia.org/wiki/Quetico_Provincial_Park">Quetico Provincial Park</a>.
+        as often as I can.
       </p>
     </Content>
   </>
@@ -75,8 +67,8 @@ const ImageGrid = ({ data }) => (
     <Slide
       img={data.quetico.childImageSharp.fluid}
       date="September 2018"
-      title="Quetico Provinial Park, Canada"
-      alt="Alex carrying a canoe and Duluth pack through a forrested portage area"
+      title="Quetico Provincial Park, Canada"
+      alt="Alex carrying a canoe and Duluth pack through a forested portage area"
     />
     <Slide
       img={data.india.childImageSharp.fluid}
