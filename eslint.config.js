9ba6ba329a73ef7ffbf324a2f8c03175da2874f0
Alex Lende <alex+github.com@lende.xyz>
2026-04-08 17:17:44 -0500

    Allow underscore prefixed unused variables
    Emulates TypeScript compiler behavior. Useful for making types more explicit.

--- eslint.config.js
+++ eslint.config.js
@@ -33,5 +33,22 @@ export default ts.config(
 				svelteConfig
 			}
 		}
+	},
+	{
+		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js', '**/*.ts', '**/*.js'],
+		rules: {
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					args: 'all',
+					argsIgnorePattern: '^_',
+					caughtErrors: 'all',
+					caughtErrorsIgnorePattern: '^_',
+					destructuredArrayIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+					ignoreRestSiblings: true
+				}
+			]
+		}
 	}
 );
