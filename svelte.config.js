c2bd6019d60c577ecbdc01bc8c59f710af4e7faa
Alex Lende <alex+github.com@lende.xyz>
2026-06-05 09:12:47 -0500

    Configure static adapter routing for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,14 @@ import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const base = process.argv.includes('dev') ? '' : (process.env.BASE_PATH ?? '');
+
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({ fallback: '404.html' }),
+		paths: { base }
+	},
 	extensions: ['.svelte', '.svx']
 };
 
